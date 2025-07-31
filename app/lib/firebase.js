6189c3bf61e69711a87f560cc4dbb4311e49f428
aviisharma238 <aviisharma238@gmail.com>
2025-07-31 20:39:48 +0530

    ✨ Updated files

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,16 +1,23 @@
-// app/lib/firebase.js
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: "codeayna",
-  storageBucket: "codeayna.firebasestorage.app",
-  messagingSenderId: "1021409351140",
-  appId: "1:1021409351140:web:d5042a00d13c06dda0a3f1",
-  measurementId: "G-EG9GDMEWK7",
-};
+let app;
+if (typeof window !== "undefined") {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: "codeayna",
+    storageBucket: "codeayna.appspot.com", // ❗fix this too (see below)
+    messagingSenderId: "1021409351140",
+    appId: "1:1021409351140:web:d5042a00d13c06dda0a3f1",
+    measurementId: "G-EG9GDMEWK7",
+  };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+  if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApps()[0];
+  }
+}
+
+export const auth = typeof window !== "undefined" ? getAuth(app) : null;
