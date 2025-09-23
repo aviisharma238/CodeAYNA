34d6aafa5a6dd4af3ae82ba3ac8109f4e9a9ff02
aviisharma238 <aviisharma238@gmail.com>
2025-09-23 20:20:14 +0530

    ✨ Updated

--- app/_component/Header.jsx
+++ app/_component/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white/10 backdrop-blur-md text-white px-6 py-4 sticky top-0 z-50 border-b border-white/20">
+     <header className="bg-black text-white px-6 py-4 sticky top-0 z-50">
       <div className="max-w-8xl mx-auto flex items-center justify-between">
         {/* Logo */}
         <Link href="/" className="text-2xl text-[#ff0080] font-extrabold">
@@ -27,7 +27,7 @@ export default function Header() {
           </Link>
           <Link
             href="/login"
-            className="bg-gray-800/60 text-sm px-4 py-2 rounded-md hover:bg-gray-700/80"
+             className="bg-gray-800 text-sm px-4 py-2 rounded-md hover:bg-gray-700"
           >
             Login
           </Link>
@@ -75,7 +75,7 @@ export default function Header() {
           </Link>
           <Link
             href="/login"
-            className="block bg-gray-800/60 text-sm px-4 py-2 rounded-md hover:bg-gray-700/80"
+             className="block bg-gray-800 text-sm px-4 py-2 rounded-md hover:bg-gray-700"
             onClick={() => setIsMenuOpen(false)}
           >
             Login
